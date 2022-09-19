d35964802858c6d3f5079d21cbd3f15307fb7c3d
Ariel Marte <ariel.amas@hotmail.com>
2022-09-19 20:47:14 -0300

    Teste Hello World!
    Organização dos arquivos e teste do Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 
-const Title = styled.h1`
-font-size: 64px;
-  color: #8257e6;
-`
+import './App.css';
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello</Title>
+      <h1 className="title">Hello World!</h1>
     </div>
   );
 }
