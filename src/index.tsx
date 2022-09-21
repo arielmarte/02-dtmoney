2bb6ef6558e45d89d775250b6ec84cb75faad7c2
Ariel Marte <ariel.amas@hotmail.com>
2022-09-21 17:08:41 -0300

    Configurando cliente

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './App';
+import {createServer} from 'miragejs';
+import {App} from './App';  
+
+createServer({
+
+  // definindo rotas
+  routes(){
+    this.namespace ='api';
+    // quando houver uma resquisição do tipo get
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount:400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
